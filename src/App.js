ef2ffda665f2cce58725c39270b696dfdab75c47
Gislaine Frandalozo <86322789+GislaineFrandalozo@users.noreply.github.com>
2022-04-22 19:14:28 -0300

    alteration h3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 // LIB
 import React from 'react';
 import RoutesForGame from './routes';
-// STYLE
-import './style/global.css'
 
 export default function App() {
     return (
--- src/components/mainAuthForm.jsx
+++ src/components/mainAuthForm.jsx
@@ -1,13 +1,15 @@
 // LIB
 import React from "react";
-import { Button, Stack } from 'react-bootstrap'
+import { Button, Stack, Col, Row } from 'react-bootstrap'
 // COMPONENTS
 import FormGame from "./form";
 
 export default function MainForm({ request, title, navigate, inputAtributes }) {
     return (
         <Stack gap={5} className="align-items-center justify-content-center">
-            <h3>{title}</h3>
+            <Col>
+                <Row className="h2 pt-4">{title}</Row>
+            </Col>
             <FormGame route={request} inputsForm={inputAtributes} />
             <Stack gap={1} className="align-items-center">
                 <p>{navigate.text}</p>
